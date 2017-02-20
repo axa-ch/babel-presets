dccda98c3c00b00f9ce756d8e3bbeffad2c83573
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-02-20 16:11:12 +0100

    export babel preset

---
+++ index.js
@@ -0,0 +1,13 @@
+modules.export = {
+  presets: [
+    'es2025'
+  ],
+
+  plugins: [
+    'transform-object-assign',
+    'transform-decorators-legacy',
+    'transform-class-properties',
+    'transform-es2015-destructuring',
+    'transform-object-rest-spread'
+  ]
+}
